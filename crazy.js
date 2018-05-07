09e3fc9c3d5faec6350e785466622b129b99d582
Gus Liakos <Gliakos@cnm.edu>
2018-05-07 14:21:29 -0600

    redo variable assignment

--- crazy.js
+++ crazy.js
@@ -1,13 +1,13 @@
 // var n1 = 1;
 // var n2 = 1;
-var btn = document.getElementsByName('button');
+var red = document.getElementsByName('button');
 
 
 function random(number) {
 	return Math.floor(Math.random() * (number + 1));
 	// return n1 = Math.floor(Math.random() * (number + 1));
 }
-	btn.onclick = function() {
+	red.onclick = function() {
 		let sizeRandomHeight = '' + random(700) + 'px';
 		let sizeRandomWidth = '' + random(700) + 'px';
 		document.body.style.height = sizeRandomHeight;
