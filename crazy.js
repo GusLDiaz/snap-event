5c5aadc50632cee6583673f33ad2b4a35ccae7a1
Gus Liakos <Gliakos@cnm.edu>
2018-05-08 16:55:12 -0600

    work from class instead of id

--- crazy.js
+++ crazy.js
@@ -1,15 +1,16 @@
 // var n1 = 1;
 // var n2 = 1;
-var blue = document.getElementById('red');
+
 
 
 function random(number) {
 	return Math.floor(Math.random() * (number + 1));
 	// return n1 = Math.floor(Math.random() * (number + 1));
 }
+let blue = document.getElementById('red');
 	blue.onclick = function() {
 		let sizeRandomHeight = '' + random(700) + 'px';
 		let sizeRandomWidth = '' + random(700) + 'px';
-		#red.style.height = sizeRandomHeight;
-		#red.body.style.width = sizeRandomWidth;
+		btn.height = sizeRandomHeight;
+		btn.width = sizeRandomWidth;
 	};
